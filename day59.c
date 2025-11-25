dfcd1b6069f2efe1d187a9ea4221fc9775d18acb
Rashi983 <rashi6083@gmail.com>
2025-11-25 19:12:12 +0530

    Add files via upload

---
+++ day59.c
@@ -0,0 +1,22 @@
+/* Q109:Write a program to take an integer array arr and an integer k as inputs. Print the maximum sum of all the subarrays of size k.
+#include <stdio.h>
+int main() {
+    int n, k;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &k);
+    int maxSum = -1000000000;  
+    for (int i = 0; i <= n - k; i++) {
+        int currentSum = 0;
+        for (int j = i; j < i + k; j++) {
+            currentSum += arr[j];
+        }
+        if (currentSum > maxSum)
+            maxSum = currentSum;
+    }
+    printf("%d", maxSum);
+    return 0;
+}
